fd8ef07bd705c462e968dec3fa2159a76498c11e
tom <generict@gmail.com>
2018-11-12 23:38:21 -0800

    initial code complete before run test

--- boids.py
+++ boids.py
@@ -26,93 +26,116 @@ class Boids:
         # maximum magnitude of the final velocity
         self.maxVel = 2.0
 
-    def tick(frameNum, pts, beak, boids):
-        #print FrameNum
-        """update function for animation"""
-        boids.tick(frameNum, pts, beak)
-        return pts, beak
+    def tick(frameNum, pts, beak):
+        """update the simulation by one time step."""
+        # get pairwise distances
+        self.distMatrix = squareform(pdist(self.pos))
+        # apply rules:
+        self.vel =+ self.applyRules()
+        self.limit(self.vel, self.maxVel)
+        self.pos += self.vel
+        self.applyBC()
+        # update data
+        pts.set_data(self.pos.reshape(2*self.N)[::2], self.pos.reshape(2 * self.N)[1::2])
+        vec = self.pos + 10 * self.vel / self.maxVel
+        beak.set_data(vec.reshape(2 * self.N)[::2], vec.reshape(2 * self.N)[1::2])
 
+    def limitVec(self, vec, maxVal):
+        """limit the magnitude of the 2D vector"""
+        mag = norm(vec)
+        if mag > maxVal:
+            vec[0], vec[1] = vec[0]*maxVal/mag, vec[1]*maxVal/mag
 
+    def limit(self, X, maxVal):
+        """limit the magnitude of 2D vectors in array X to maxValue"""
+        for vec in X:
+            self.limitVec(vec, maxVal)
 
-def applyBC(self):
-    """apply boundry conditions"""
-    deltaR = 2.0
-    for coord in self.pos:
-        if coord[0] > width + deltaR:
-            coord[0] = - deltaR
-        if coord[0] < - deltaR:
-            coord[0] = width + deltaR
-        if coord[1] > height + deltaR:
-            coord[1] = - deltaR
-        if coord[1] < - deltaR:
-            coord[1] = height + deltaR
+    def applyBC(self):
+        """apply boundry conditions"""
+        deltaR = 2.0
+        for coord in self.pos:
+            if coord[0] > width + deltaR:
+                coord[0] = - deltaR
+            if coord[0] < - deltaR:
+                coord[0] = width + deltaR
+            if coord[1] > height + deltaR:
+                coord[1] = - deltaR
+            if coord[1] < - deltaR:
+                coord[1] = height + deltaR
 
-fig = plt.figure()
-ax = plt.axes(xlim=(0,width), ylim=(0,height))
+    def applyRules(self):
+        #Rule One - Separation
+        D = distMatrix < 25.0
+        vel = self.pos*D.sum(axis=1).reshape(self.N, 1) - D.dot(self.pos)
+        self.limit(vel, self.maxRuleVel)
 
-pts, = ax.plot([],[],markersize=10, c='k', marker='o', ls='None')
-beak, = ax.plot([],[],markersize=4, c='r', marker='o', ls='None')
-anim = animation.FuncAnimation(fig, tick, fargs=(pts, beak, boids), interval=50)
+        #distance threshold for alignment
+        D = distMatrix < 50.0
 
-vec = self.pos + 10*self.vel/self.maxVel
-beak.set_data(vec.reshape(2*self.N)[::2], ved.reshape(2*self.N)[1::2])
+        #apply rule two - alignment
+        vel2 = D.dot(self.vel)
+        self.limit(vel2, self.maxRuleVel)
+        vel += vel2;
 
-def test2(pos, radius):
-    distMatrix = squareform(pdist(pos))
-    D = distMatrix < radius
-    vel = pos*D.sum(axis=1).reshape(N,1) - D.dot(pos)
-    return vel
+        #apply rule 3 - cohesino
+        vel3 = D.dot(self.pos) - self.pos
+        self.limit(vel3, self.maxRuleVel)
+        vel += vel3
 
-def applyRules(self):
-    #Rule One - Separation
-    D = distMatrix < 25.0
-    vel = self.pos*D.sum(axis=1).reshape(self.N, 1) - D.dot(self.pos)
-    self.limit(vel, self.maxRuleVel)
+        return vel
 
-    #distance threshold for alignment
-    D = distMatrix < 50.0
+    def buttonPress(self, event):
+        """event handelr for matplotlib button presses"""
+        # left-click to add a boid
+        if event.button is 1:
+            self.pos = np.concatenate((self.pos, np.array([[event.xdata, event.ydata]])), axis=0)
+            # generate random velocity
+            angles = 2 * math.pi * np.random.rand(1)
+            v = np.array(list(zip(np.sin(angles), np.cos(angles))))
+            self.vel = np.concatenate((self.vel, v), axis=0)
+            self.N += 1
+        # right-click to scatter boids
+        elif event.button is 3:
+            # add scattering velocity
+            self.vel += 0.1 * (self.pos - np.array([[event.xdata, event.ydata]]))
 
-    #apply rule two - alignment
-    vel2 = D.dot(self.vel)
-    self.limit(vel2, self.maxRuleVel)
-    vel += vel2;
+def tick(frameNum, pts, beak, boids):
+    boids.tick(frameNum, pts, boids)
+    return pts, beak
 
-    #apply rule 3 - cohesino
-    vel3 = D.dot(self.pos) - self.pos
-    self.limit(vel3, self.maxRuleVel)
-    vel += vel3
+# main() function
+def main():
+    # use sys.argv if needed
+    print('starting boids...')
 
-    return vel
+    parser = argparse.ArgumentParser(description="Implementing Craig Reynolds's Boids...")
 
-# add a "button press" event handler
-cid = fig.canvas.mpl_connect('button_press_event', buttonPress)
+    # add arguments
+    parser.add_argument('--num-boids', dest='N', required=False)
+    args = parser.parse_args()
 
-def buttonPress(self, event):
-    """event handelr for matplotlib button presses"""
-    # left-click to add a boid
-    if event.button is 1:
-        self.pos = np.concatenate((self.pos, np.array([[event.xdata, event.ydata]])), axis=0)
-        # generate random velocity
-        angles = 2*math.pi*np.random.rand(1)
-        v = np.array(list(zip(np.sin(angles), np.cos(angles))))
-        self.vel = np.concatenate((self.vel, v), axis=0)
-        self.N += 1
-    # right-click to scatter boids
-    elif event.button is 3:
-        # add scattering velocity
-        self.vel += 0.1*(self.pos - np.array([[event.xdata, event.ydata]]))
+    # set the initial number of boids
+    N = 100
+    if args.N:
+        n = int(args.N)
 
-parser = argpars.ArgumentParser(description="Implementing Craig Reynolds's Boids...")
+    # create boids
+    boids = Boids(N)
 
-# add arguments
-parser.add_argument('--num-boids', dest='N', required=False)
-args = parser.parse_args()
+    # set up plot
+    fig = plt.figure()
+    ax = plt.axes(xlim=(0,width), ylim=(0,height))
 
-# set the initial number of boids
-N = 100
-if args.N:
-    n = int(args.N)
+    pts, = ax.plot([],[],markersize=10, c='k', marker='o', ls='None')
+    beak, = ax.plot([],[],markersize=4, c='r', marker='o', ls='None')
+    anim = animation.FuncAnimation(fig, tick, fargs=(pts, beak, boids), interval=50)
 
-# create boids
-boids = Boids(N)
+    # add a "button press" event handler
+    cid = fig.canvas.mpl_connect('button_press_event', buttonPress)
 
+    plt.show()
+
+# call main
+if __name__ == '__main__':
+    main()
